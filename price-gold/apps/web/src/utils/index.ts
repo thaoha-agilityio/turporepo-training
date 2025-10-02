850d35870c81d0926d701f7faa8763c53404d8ef
Thao Ha <thao.ha@asnet.com.vn>
2025-10-02 10:58:45 +0700

    feat: remove redundant code

--- price-gold/apps/web/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './chart';
-export * from './cn';
-export * from './format';
-export * from './notification';
-export * from './platformInfo';
--- price-gold/apps/web/src/utils/platformInfo.ts
+++
@@ -1,5 +0,0 @@
-export const isStandalone = window.matchMedia(
-  '(display-mode: standalone)',
-).matches;
-
-export const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
